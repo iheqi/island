9aad33bbb3c48acef3ba66530a64c7be1b77f889
rubbish-man <993284758@qq.com>
2019-07-28 18:06:39 +0800

    全局异常处理

--- api/v1/book.js
+++ api/v1/book.js
@@ -1,10 +1,17 @@
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/v1/book/latest', (ctx, next) => {
+router.post('/v1/book/latest', (ctx, next) => {
+	const path = ctx.params;
+	const query = ctx.request.query;
+	const headers = ctx.request.header;
+	const body = ctx.request.body;
+
 	ctx.body = {
 		key: 'book'
 	}
+
+	throw new Error('为什么还是报错error');
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const Koa = require('koa');
+const parser = require('koa-bodyparser');
 const InitManager = require('./core/init');
+const catchError = require('./middlewares/exception');
+
 const app = new Koa();
 
-
+app.use(catchError);
 InitManager.initCore(app);
+app.use(parser());
 console.log('fuck');
 
 app.listen(3000);
